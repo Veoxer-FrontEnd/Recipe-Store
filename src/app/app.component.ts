78fbcc510634d1267b40f19c2fa05d8b6d458236
Si Mohamed Hadrach <mohamed.hadrach@gmail.com>
2020-02-28 21:14:36 +0100

    setting up the app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // loadedSection: string = 'recipes';
-  // title = 'Recipe Store';
+  title = 'Recipe Store';
 
   constructor(){}
 
